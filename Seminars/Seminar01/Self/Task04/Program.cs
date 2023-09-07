229d63089c7fb01b01408c47015a8f817a782bb8
xhezh <144353105+xhezh@users.noreply.github.com>
2023-09-07 17:53:01 +0300

    Update Program.cs

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,12 @@
-﻿// Your code here!
+using System;
+
+class LoL { 
+    static void Main() {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Black;
+
+        string un = Console.ReadLine();
+        
+        Console.WriteLine("Привет, " + un + '!');
+    }
+}
