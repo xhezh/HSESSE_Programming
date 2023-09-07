ec55a191542ff1605e99246a7cc31caf70dbfcbc
xhezh <144353105+xhezh@users.noreply.github.com>
2023-09-07 17:28:55 +0300

    Update Program.cs

--- Seminars/Seminar01/Self/Task01/Program.cs
+++ Seminars/Seminar01/Self/Task01/Program.cs
@@ -1,1 +1,10 @@
-﻿// Your code here!
+using System;
+class LoL { 
+    static void Main() {
+        string fam = Console.ReadLine(), nam = Console.ReadLine(), sur = Console.ReadLine();
+        
+        Console.WriteLine("Фамилия: " + fam);
+        Console.WriteLine("Имя: " + nam);
+        Console.WriteLine("Отчество: " + sur);
+    }
+}
