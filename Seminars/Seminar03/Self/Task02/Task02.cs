feb2fc73a8991e5b455bb29481ff04e42d4bba14
Федянин Валерий <144353105+xhezh@users.noreply.github.com>
2023-09-16 14:05:17 +0300

    Create Task02.cs

---
+++ Seminars/Seminar03/Self/Task02/Task02.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks.Dataflow;
+using System;
+
+class LoL { 
+    static void Main() {
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+
+        if (x * x + y * y > 1 * 1 && x * x + y * y < 2 * 2 && y > 0) {
+            Console.WriteLine("Точка принадлежит области");
+        }
+        else if ((x * x + y * y == 1 * 1 || x * x + y * y == 2 * 2) && y >= 0 || y == 0 && (x <= 2 && x >= 1 || x >= -2 && x <= -1)) {
+            Console.WriteLine("Точка принадлежит границе");
+        }
+        else {
+            Console.WriteLine("Точка не принадлежит области");
+        }
+    }
+}
