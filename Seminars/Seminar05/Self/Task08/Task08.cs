eafd60b8c7122987dcf8842ac6a6ec6ffcea4d25
Федянин Валерий <144353105+xhezh@users.noreply.github.com>
2023-09-20 16:37:13 +0300

    Create Task08.cs

---
+++ Seminars/Seminar05/Self/Task08/Task08.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks.Dataflow;
+using System;
+using System.Xml.Serialization;
+
+class LoL { 
+    static void Main() {
+        int x = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+        if (Math.Pow(10, k - 1) > x) {
+            Console.WriteLine("K не должно превышать количество разрядов исходного числа.");
+        }
+        else {
+            while (Math.Pow(10, k) < x) {
+                x /= 10;
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
