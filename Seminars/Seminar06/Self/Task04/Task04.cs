066aeb9b3e64ec5c5e2ac8f500b00c288603b70a
Федянин Валерий <144353105+xhezh@users.noreply.github.com>
2023-09-20 17:16:20 +0300

    Create Task04.cs

---
+++ Seminars/Seminar06/Self/Task04/Task04.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks.Dataflow;
+using System;
+using System.Xml.Serialization;
+
+
+class LoL { 
+    static void Main() {
+        Console.WriteLine((((( 1 + 2) * 3) * 4) + 5) - 6);
+    }
+}
